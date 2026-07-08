214a407a6b4d2f1dbc4c403d322a91084b930a85
William Paiva <33769441+quakeman22@users.noreply.github.com>
2026-07-08 13:03:10 -0300

    Add files via upload

--- app/src/main/java/javax/microedition/shell/MicroActivity.java
+++ app/src/main/java/javax/microedition/shell/MicroActivity.java
@@ -43,6 +43,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.Surface;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
