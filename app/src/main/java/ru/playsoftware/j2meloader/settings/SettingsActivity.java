23d799d49be5d29bc2a32b76c6e353e3da6d0f29
William Paiva <33769441+quakeman22@users.noreply.github.com>
2026-07-09 07:58:03 -0300

    Add files via upload

--- app/src/main/java/ru/playsoftware/j2meloader/settings/SettingsActivity.java
+++ app/src/main/java/ru/playsoftware/j2meloader/settings/SettingsActivity.java
@@ -16,11 +16,16 @@
 
 package ru.playsoftware.j2meloader.settings;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 
 import ru.playsoftware.j2meloader.R;
 
@@ -33,5 +38,31 @@ public class SettingsActivity extends AppCompatActivity {
 		Toolbar toolbar = findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
 		toolbar.setNavigationOnClickListener(v -> finish());
+		hideSystemUI();
+	}
+
+	@Override
+	public void onWindowFocusChanged(boolean hasFocus) {
+		super.onWindowFocusChanged(hasFocus);
+		if (hasFocus) {
+			hideSystemUI();
+		}
+	}
+
+	private void hideSystemUI() {
+		WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+		WindowInsetsControllerCompat controller =
+				WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView());
+		if (controller != null) {
+			controller.setSystemBarsBehavior(
+					WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+			controller.hide(WindowInsetsCompat.Type.statusBars() | WindowInsetsCompat.Type.navigationBars());
+		}
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+			WindowManager.LayoutParams attributes = getWindow().getAttributes();
+			attributes.layoutInDisplayCutoutMode =
+					WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+			getWindow().setAttributes(attributes);
+		}
 	}
 }
