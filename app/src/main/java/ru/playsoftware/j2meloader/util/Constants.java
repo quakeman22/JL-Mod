13aa8a621eb7fadb996b4a5015a127444824a94f
William Paiva <33769441+quakeman22@users.noreply.github.com>
2026-07-08 21:37:48 -0300

    Add files via upload

--- app/src/main/java/ru/playsoftware/j2meloader/util/Constants.java
+++ app/src/main/java/ru/playsoftware/j2meloader/util/Constants.java
@@ -29,6 +29,7 @@ public class Constants {
 	public static final String PREF_APPS_VIEW = "pref_apps_view";
 	public static final String PREF_DEFAULT_PROFILE = "default_profile";
 	public static final String PREF_EMULATOR_DIR = "emulator_dir";
+	public static final String PREF_CLASSICS_VIEW_SIZE = "pref_classics_view_size";
 	public static final String PREF_KEEP_SCREEN = "pref_wakelock_switch";
 	public static final String PREF_LAST_PATH = "pref_last_path";
 	public static final String PREF_STATUSBAR = "pref_statusbar_switch";
