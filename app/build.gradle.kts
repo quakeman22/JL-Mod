2ade14aded443f788b0a7da3791b8cdac7f34d82
William Paiva <33769441+quakeman22@users.noreply.github.com>
2026-07-09 08:36:18 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -93,10 +93,7 @@ android {
     }
 
     splits.abi {
-    isEnable = true
-    reset()
-    include("x86", "armeabi-v7a", "x86_64", "arm64-v8a")
-    isUniversalApk = true
+    isEnable = false
 }
 
     externalNativeBuild.ndkBuild.path("src/main/cpp/Android.mk")
