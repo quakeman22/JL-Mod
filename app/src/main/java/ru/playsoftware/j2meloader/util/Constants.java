8c2b0984423b396b338d041a3d6c361acec669f5
William Paiva <33769441+quakeman22@users.noreply.github.com>
2026-07-11 19:17:06 -0300

    Atualizar o Constants.java

--- app/src/main/java/ru/playsoftware/j2meloader/util/Constants.java
+++ app/src/main/java/ru/playsoftware/j2meloader/util/Constants.java
@@ -28,6 +28,7 @@ public class Constants {
 	public static final String PREF_APP_SORT = "pref_app_sort";
 	public static final String PREF_APPS_VIEW = "pref_apps_view";
 	public static final String PREF_CLASSICS_CONTROL_STYLE = "pref_classics_control_style";
+	public static final String PREF_CLASSICS_HANDSET_SKIN = "pref_classics_handset_skin";
 	public static final String PREF_DEFAULT_PROFILE = "default_profile";
 	public static final String PREF_EMULATOR_DIR = "emulator_dir";
 	public static final String PREF_CLASSICS_VIEW_SIZE = "pref_classics_view_size";
