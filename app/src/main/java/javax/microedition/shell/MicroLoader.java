93ddfc287491a6b6675f6cfc0effa77b07684476
William Paiva <33769441+quakeman22@users.noreply.github.com>
2026-07-13 07:43:02 -0300

    Add files via upload

--- app/src/main/java/javax/microedition/shell/MicroLoader.java
+++ app/src/main/java/javax/microedition/shell/MicroLoader.java
@@ -264,6 +264,7 @@ public class MicroLoader {
 
 			Displayable.setVirtualSize(params.screenWidth, params.screenHeight);
 			EventQueue.setImmediate(params.immediateMode);
+			EventQueue.setImmediateInput(params.immediateInputMode);
 			ShaderInfo shader = params.shader;
 			if (shader != null) {
 				shader.dir = workDir + Config.SHADERS_DIR;
